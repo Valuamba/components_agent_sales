fcba355ab0f2651d678904b3d6ac3b389f506008
valuamba <valentin.marchuk20@gmail.com>
2024-04-14 18:20:31 +0300

    add crm admin docker

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -16,16 +16,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nl)mz*=h^h9n!z1734%*x+^y#cc+i@$m5+nxgmv#d@&#izh=-#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
